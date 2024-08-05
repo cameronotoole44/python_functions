c395c311f1f4e9850370f368cf9898259c78d1f1
cameronotoole44 <cameronotoole404@gmail.com>
2024-08-05 18:58:21 -0400

    pain

--- more_function_practice.py
+++ more_function_practice.py
@@ -53,44 +53,38 @@ print(num_within(24,2,6))
 
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
 
-def pascal(num):
-    if num < 1:
-        print("invalid number of rows")
-       
 
+triangle = [[1],[1,1]]
+def pascal(n):
+  #base case
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    #fill up correct number of rows in triangle
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      #create correct row, then add to triangle (this row will be 1 longer than row before it)
+      length = len(row_prev)+1
+      for i in range(length):
+        #first number is 1
+        if i == 0:
+          row.append(1)
+        #intermediate numbers get added from previous rows
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        #last number is 1
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
 
+    #print triangle
+    for row in triangle:
+      print(row)
 
-# triangle = [[1],[1,1]]
-# def pascal(n):
-#   #base case
-#   if n < 1:
-#     print("invalid number of rows")
-#   elif n == 1:
-#     print(triangle[0])
-#   else:
-#     row_number = 2
-#     #fill up correct number of rows in triangle
-#     while len(triangle) < n:
-#       row = []
-#       row_prev = triangle[row_number - 1]
-#       #create correct row, then add to triangle (this row will be 1 longer than row before it)
-#       length = len(row_prev)+1
-#       for i in range(length):
-#         #first number is 1
-#         if i == 0:
-#           row.append(1)
-#         #intermediate numbers get added from previous rows
-#         elif i > 0 and i < length-1:
-#           row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
-#         #last number is 1
-#         else:
-#           row.append(1)
-#       triangle.append(row)
-#       row_number += 1
-
-#     #print triangle
-#     for row in triangle:
-#       print(row)
-
-# pascal(2)
-# pascal(5)
+pascal(2)
+pascal(5)
